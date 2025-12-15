5215bf01226355030b7af82b6c3e938a94fc66e8
Akshai Asok <akshai@example.com>
2025-12-15 21:58:23 +0530

    hgj

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,7 +2,7 @@ import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
     name: 'Akshai Asok',
-    title: "Web Developer, Cybersecurity Enthusiast & OdOO Developer",
+    title: "Cybersecurity Enthusiast, Web Developer & OdOO Developer",
     desciption:"The sky whispered of change — not through thunder, but through color. Even the clouds knew that transformation begins quietly.",
     image: '',
     resumePdf: resume
