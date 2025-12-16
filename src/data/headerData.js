861e0901eb8ef1e3cb9070f7c0110af40c7c78c2
Akshai Asok <akshai@example.com>
2025-12-16 22:08:04 +0530

    ressume update

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
-import resume from '../assets/pdf/resume.pdf'
-
 export const headerData = {
     name: 'Akshai Asok',
-    title: "Cybersecurity Enthusiast, Web Developer & OdOO Developer",
-    desciption:"The sky whispered of change — not through thunder, but through color. Even the clouds knew that transformation begins quietly.",
+    title: 'Cybersecurity Enthusiast, Web Developer & Odoo Developer',
+    desciption:
+        'The sky whispered of change — not through thunder, but through color. Even the clouds knew that transformation begins quietly.',
     image: '',
-    resumePdf: resume
-}
+    resumePdf:
+        'https://drive.google.com/uc?export=download&id=1QA3Px6RBO4CP2YSZxVtLvRbe4s5asCZ5'
+};
