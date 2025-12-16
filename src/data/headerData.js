79a345c532af156cde2cff85623d0a5d06a26afe
Akshai Asok <akshai@example.com>
2025-12-16 22:35:50 +0530

    image correction

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,10 @@
+import myImage from '../assets/Me/njn.jpg';
 export const headerData = {
     name: 'Akshai Asok',
     title: 'Cybersecurity Enthusiast, Web Developer & Odoo Developer',
     desciption:
         'The sky whispered of change — not through thunder, but through color. Even the clouds knew that transformation begins quietly.',
-    image: '',
+    image: myImage,
     resumePdf:
         'https://drive.google.com/uc?export=download&id=1QA3Px6RBO4CP2YSZxVtLvRbe4s5asCZ5'
 };
